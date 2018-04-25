c6ee9c612a1ca08354d8574d7d0f7a284fe5fe86
Velverto <35104407+Velverto@users.noreply.github.com>
2018-04-25 23:07:59 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,10 @@
 *
 */
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -51,6 +53,7 @@ public class Main {
 	    			case 2:
 	    				//Run the simulation from a file
 	    				System.out.println("Please include the full path of the file:");
+	    				System.out.println("If successful, a file output.txt is generated at the application directory. It contain the final position of the mowers");
 	    				input_string = in.readLine();
 	    				try {
 	    					in_file = new BufferedReader(new FileReader(new File(input_string)));
@@ -122,82 +125,101 @@ public class Main {
     	Field main_field;
 	    Mower mower;
 	    String input_string;
+	    BufferedWriter output_file;
 	    
-	    /******	First line is the coordinates of the upper right corner of the lawn
-    	 * 		The program will create a field using these coordinates
-    	 */
 	    try {
-	    	input_string = input_file.readLine();
-	    	if(input_string == null) {
-	    		System.out.println("Data Error: Data not found at line 1 !");
-	    		return;
-	    	}
-	    		//Locate the space between 2 numbers, 
-	    		//if there is no space then the test case is invalid
-	    		space_index1 = input_string.indexOf(' ', 0);
-	    		if(space_index1 == -1) {
-	    			System.out.println("Data Error: Invalid format at line 1 !");
-	    			return;
-	    		}
-	    		else {
-	    			try {
-		    			//Initiate the main field
-		    		    x = Integer.parseInt(input_string.substring(0, space_index1));
-		    			y = Integer.parseInt(input_string.substring(space_index1+1, input_string.length()));
-		    			main_field = new Field(x, y);
-		    			System.out.println("Field initialized!");
-	    			}
-	    			catch(NumberFormatException e) {
-	    				System.out.println("Data Error: Invalid value at line 1 !");
-	    				return;
-	    			}
-	    		}
-	    			
-	    	/*****Mower commands lines************/
-	    	int mower_counter = 1;
-	    	while((input_string = input_file.readLine()) != null) {  
-	    		/***Line 1: initiate the mower
-	    		 * the format is x y Direction
-	    		 * Exemple: 1 2 N
-	    		 */
-	    		space_index1 = input_string.indexOf(' ', 0);
-	    		space_index2 = input_string.indexOf(' ', space_index1+1);
-	    		if(space_index1 == -1 || space_index2 == -1) {
-	    			System.out.println("Data Error: Invalid format at line " + 2*mower_counter + " !");
-	    			return;
-	    		}
-	    		else {
-	    			try {
-		    		    x = Integer.parseInt(input_string.substring(0, space_index1));
-		    			y = Integer.parseInt(input_string.substring(space_index1+1, space_index2));
-	    			}
-	    			catch(NumberFormatException e) {
-	    				e.printStackTrace();
-	    				return;
-	    			}
-	    			String tmp_str = input_string.substring(space_index2+1, input_string.length());
-	    			direction = tmp_str.charAt(0);
-	    			mower = new Mower(x, y, direction, main_field);
-	    			System.out.println("Mower " + mower_counter + " initialized at: " + mower.getPos_x() + " " + mower.getPox_y() + " " + mower.getDirection());
-	    		}
-	    		
-		    	/***Line 2: Mower commands*****/
+	    	output_file = new BufferedWriter(new FileWriter(new File("output.txt")));
+	    
+		    /******	First line is the coordinates of the upper right corner of the lawn
+	    	 * 		The program will create a field using these coordinates
+	    	 */
+		    try {
 		    	input_string = input_file.readLine();
-			    if(input_string == null) {
-			    	System.out.println("Data Error: Invalid format at line " + ((2*mower_counter)+1) + " !");
-			    	return;
-			    }
-		    	for(char i : input_string.toCharArray()) {
-		    		mower.move(main_field, i);
+		    	if(input_string == null) {
+		    		System.out.println("Data Error: Data not found at line 1 !");
+		    		output_file.close();
+		    		return;
 		    	}
-		    	System.out.println("Moving sequences completed!");
-		    	System.out.println("Mower " + mower_counter + " final position: " + mower.getPos_x() + " " + mower.getPox_y() + " " + mower.getDirection());
-		    	mower_counter++;
-	    	}
+		    		//Locate the space between 2 numbers, 
+		    		//if there is no space then the test case is invalid
+		    		space_index1 = input_string.indexOf(' ', 0);
+		    		if(space_index1 == -1) {
+		    			System.out.println("Data Error: Invalid format at line 1 !");
+		    			output_file.close();
+		    			return;
+		    		}
+		    		else {
+		    			try {
+			    			//Initiate the main field
+			    		    x = Integer.parseInt(input_string.substring(0, space_index1));
+			    			y = Integer.parseInt(input_string.substring(space_index1+1, input_string.length()));
+			    			main_field = new Field(x, y);
+			    			System.out.println("Field initialized!");
+		    			}
+		    			catch(NumberFormatException e) {
+		    				System.out.println("Data Error: Invalid value at line 1 !");
+		    				output_file.close();
+		    				return;
+		    			}
+		    		}
+		    			
+		    	/*****Mower commands lines************/
+		    	int mower_counter = 1;
+		    	while((input_string = input_file.readLine()) != null) {  
+		    		/***Line 1: initiate the mower
+		    		 * the format is x y Direction
+		    		 * Exemple: 1 2 N
+		    		 */
+		    		space_index1 = input_string.indexOf(' ', 0);
+		    		space_index2 = input_string.indexOf(' ', space_index1+1);
+		    		if(space_index1 == -1 || space_index2 == -1) {
+		    			System.out.println("Data Error: Invalid format at line " + 2*mower_counter + " !");
+		    			output_file.close();
+		    			return;
+		    		}
+		    		else {
+		    			try {
+			    		    x = Integer.parseInt(input_string.substring(0, space_index1));
+			    			y = Integer.parseInt(input_string.substring(space_index1+1, space_index2));
+		    			}
+		    			catch(NumberFormatException e) {
+		    				System.out.println("Data Error: Invalid value at line " + 2*mower_counter + " !");
+		    				output_file.close();
+		    				return;
+		    			}
+		    			String tmp_str = input_string.substring(space_index2+1, input_string.length());
+		    			direction = tmp_str.charAt(0);
+		    			mower = new Mower(x, y, direction, main_field);
+		    			System.out.println("Mower " + mower_counter + " initialized at: " + mower.getPos_x() + " " + mower.getPox_y() + " " + mower.getDirection());
+		    		}
+		    		
+			    	/***Line 2: Mower commands*****/
+			    	input_string = input_file.readLine();
+				    if(input_string == null) {
+				    	System.out.println("Data Error: Invalid format at line " + ((2*mower_counter)+1) + " !");
+				    	output_file.close();
+				    	return;
+				    }
+			    	for(char i : input_string.toCharArray()) {
+			    		mower.move(main_field, i);
+			    	}
+			    	System.out.println("Moving sequences completed!");
+			    	System.out.println("Mower " + mower_counter + " final position: " + mower.getPos_x() + " " + mower.getPox_y() + " " + mower.getDirection());
+			    	System.out.println("");
+			    	//save the results to output.txt
+			    	output_file.write(mower.getPos_x() + " " + mower.getPox_y() + " " + mower.getDirection() + "\n");
+			    	mower_counter++;
+		    	}
+		    	output_file.close();
+		    }
+		    catch(IOException e_input) {
+		    	System.out.println("Data Error: IOException!");
+		    	e_input.printStackTrace();
+		    }
 	    }
-	    catch(IOException e) {
-	    	System.out.println("Data Error: IOException!");
-	    	e.printStackTrace();
+	    catch(IOException e_output) {
+	    	System.out.println("Error: Cannot create file output.txt, make sure the name output.txt and/or the permission is avaiable");
+	    	e_output.printStackTrace();
 	    }
 		return;
 	}
